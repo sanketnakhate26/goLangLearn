0708722919f01f7dac566c63750cd8aae9279349
snakhate <snakhate@cisco.com>
2020-08-23 22:59:36 +0530

    added comments

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,14 @@
+// Program to print the Hello World on to the console.
+
+// Declaring package main to group together the code into the same package.
 package main
 
+// To import the predefine library / package fmt which is having the declaration of Println function.
 import "fmt"
 
+// Declairation of the main fuction, where the execution will start.
 func main() {
 	fmt.Println("Hello World !!!")
 }
+
+// End of the Program
