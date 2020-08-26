19af2e303622ceb1084f5752a4ff9d89ca7169d3
snakhate <snakhate@cisco.com>
2020-08-26 16:17:26 +0530

    program updated

---
+++ practicePrograms/fileOperations.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+import "io/ioutil"
+import "strings"
+
+type students []string
+func main(){
+	stu := initStudents()
+	fmt.Println("File contents before save : ")
+	fmt.Println(stu)
+	fmt.Println("Saving file.....")
+	stu.saveToFile("new.txt")
+	fmt.Println("Reading contents from the saved file :")
+	fmt.Println(newDeckFromFile("new.txt"))
+
+
+}
+func initStudents() students{
+	return []string{"Sanket","Raj","Rohit","Gargi"}
+}
+
+func (s students) saveToFile(filename string) error{
+	return ioutil.WriteFile(filename,[]byte(s.toJoinString()),0666)
+}
+
+func (s students) toJoinString() string{
+	return strings.Join([]string(s),",")
+}
+
+func newDeckFromFile(filename string) students {
+	bs, err := ioutil.ReadFile("new.txt")
+	if err != nil{
+		fmt.Println(err)
+		os.Exit(1)
+		}
+	s := strings.Split(string(bs),",")
+	return students(s)
+}
